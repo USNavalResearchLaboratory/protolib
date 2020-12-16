06c9081eef9af6ca26b53b17a4146962e90ff1af
Caleb Bowers <calebbowers@MacBook-Pro.local>
2020-12-16 10:34:00 -0500

    CZB. Protokit.cpp cleanup

--- src/python/protokit.cpp
+++ src/python/protokit.cpp
@@ -68,43 +68,4 @@ moduleinit(void)
 #endif
 
 
-
-
-
-
-
-// #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
-// #define PyMODINIT_FUNC void
-// #endif
-//
-// PyMODINIT_FUNC
-// #if PY_MAJOR_VERSION >= 3
-// PyInit_protokit(void)
-// #else
-// initprotokit(void)
-// #endif
-//     {
-//         PyObject* m;
-//
-//         if (PyType_Ready(&PipeType) < 0)
-//             return;
-//
-// #if PY_MAJOR_VERSION >= 3
-//
-//         m = PyModule_Create(&moduledef);
-// #else
-//         m = Py_InitModule3("protokit", protokit_methods, "Python wrapper for Protokit");
-//
-// #endif
-//         if (m == NULL)
-//             return;
-//
-//         ProtoError = PyErr_NewException((char*)"protokit.ProtoError", NULL, NULL);
-//         Py_INCREF(ProtoError);
-//         PyModule_AddObject(m, "ProtoError", ProtoError);
-//
-//         Py_INCREF(&PipeType);
-//         PyModule_AddObject(m, "Pipe", (PyObject *)&PipeType);
-//     }
-
 } // extern "C"
